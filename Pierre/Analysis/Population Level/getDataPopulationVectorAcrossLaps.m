1be54aa033a4a09bd2f623f585ceebfc55eed3a3
Pierre Var <78429558+tarsky5@users.noreply.github.com>
2024-01-23 12:04:33 +0000

    end script generate PV data FPF

--- Pierre/Analysis/Population Level/getDataPopulationVectorAcrossLaps.m
+++ Pierre/Analysis/Population Level/getDataPopulationVectorAcrossLaps.m
@@ -12,126 +12,104 @@
 
 clear
 
+% PATH things
+PATH.SCRIPT = fileparts(mfilename('fullpath'));
+cd(PATH.SCRIPT)
+
 sessions = data_folders_excl; % Use the function to get all the file paths
 
-finalData = struct("condition", {}, "session", {}, "lap", {}, "PV_cor", {});
-
-% Mode of calculation : 
-% "LAST" : last lap re-exposure
-% "FIRST" : first lap exposure
-% "ALL" : all laps re-exposure
-% "NEXT" : next lap
-
-mode = "NEXT";
-
-% We iterate through all the sessions
-
-for cfile = sessions(1)
+population_vector_laps = struct("animal", {}, "condition", {}, "day", {}, ...
+                       "track", {}, "allLaps", {}, "finalPlaceField", {});
+                   
+allLaps = struct("lap", {}, "pvCorrelation", {}, "euclidianDistance", {}, "cosineSim", {});
+                              
+% We iterate through files
+for cfile = sessions
     disp(cfile);
     file = cfile{1};
-
+    
     [animalOI, conditionOI, dayOI] = parseNameFile(file); % We get the informations about the current data
 
+    %% Variable loading
     % We load the place fields computed per lap for each animal - lap_place_fields
     load(file + "\extracted_lap_place_fields");
-
-    % Exit parameter if the treatment is impossible (e.g. NEXT mode + 16x1)) 
-    shouldPass = false;
-
-    % We iterate through tracks
-    for track = 1:4
-
-        % We create a variable to keep track of if the current track is
-        % an exposition or a re-exposition
-
-        expSession = 1 * (track > 2) + 1;
-
-        %% Defining the current and concurrent PF data
-        if mode == "LAST"
-            currentPFData = lap_place_fields(track).Complete_Lap;
-            concurrentPFData = lap_place_fields(track + expSession).Complete_Lap;
+    % And a general PF data to check if good cell on the whole track
+    load(file + "\extracted_place_fields");
+    load(file + "\extracted_laps"); % Import table lap_times
+    load(file + "\extracted_clusters"); % Import table clusters
+    load(file + "\extracted_waveforms"); % Import allclusters_waveform
+    load(file + "\extracted_position"); % Import position
+    
+    %% We compute the final place field
+    
+    % Definition : place field of the 16 last lap of RUN2.
+    
+    % We find the common number of laps on T3 - RUN2 between the files
+    totalLapsRUN2 = min(lap_times(3).number_completeLaps, length(lap_place_fields(3).Complete_Lap));
+    
+    % We find the start / end times of the 16 last laps
+    
+    startTime = lap_times(3).completeLaps_start(totalLapsRUN2 - 16);
+    endTime = lap_times(3).completeLaps_stop(totalLapsRUN2);
+    
+    % We then crop the position mat, and compute the place field
+    mutPositions = cropPositionsAtTime(position, 3, startTime, endTime);
+    
+    finalPlaceField = calculate_place_fields_RD(2, mutPositions, clusters, allclusters_waveform);
+    finalPlaceField = finalPlaceField.track(3);
+    
+    % For population vector analysis, we only use the good place cells of
+    % the FPF - cells that will become good cells
+    
+    goodCells = finalPlaceField.good_cells;
+    
+    % We register the number of laps now available for RUN2 decoding
+    
+    remainingLapsRUN2 = totalLapsRUN2 - 16;
+       
+    % We iterate over tracks 1 and 3
+    for track = 1:2:3
         
-        % If mode == "FIRST", we use the same data for current and concurrent
-        elseif mode == "FIRST"
-            currentPFData = lap_place_fields(track).Complete_Lap;
-            concurrentPFData = currentPFData;
+        % We find the number of laps
+        nbLaps = min([lap_times(track).number_completeLaps, length(lap_place_fields(track).Complete_Lap)]);
         
-        % If mode == "ALL", we use global place field computed on all of track 3/4
-        elseif mode == "ALL"
-            currentPFData = lap_place_fields(track).Complete_Lap;
-            load(file + "\extracted_place_fields");
-            concurrentPFData = place_fields.track(track + expSession);
+        % We create the struct to store the data per lap
         
-        % If mode == "NEXT", we wont use conditions "16x1" (no next lap in that case)
-        elseif mode == "NEXT"
-            if conditionOI == "16x1"
-                shouldPass = true;
-                continue % Stop the track iteration loop
-            else
-                currentPFData = lap_place_fields(track).Complete_Lap;
-                concurrentPFData = currentPFData;
+        allLaps = struct("lap", {}, "pvCorrelation", {}, "euclidianDistance", {}, "cosineSim", {});                
+               
+        % We iterate through laps
+        for lap = 1:nbLaps
+            
+            % We get the relevant data regarding place fields
+            goodPFData = lap_place_fields(track).Complete_Lap{lap};
+            
+            currentPFCellArray = goodPFData.raw;
+            concurrentCellArray = finalPlaceField.raw;
+            
+            % rare case : if not the same resolution, for now we ignore
+            if(length(currentPFCellArray{1}) ~= length(concurrentCellArray{1}))
+                continue;
             end
+            
+            % We find all the goodCells of the FPF that are in 
+            
+            % We compute the PV correlation
+            pvCorrelation = getPVCor(goodCells, currentPFCellArray, concurrentCellArray, "pvCorrelation");
+            euclidianDistance = getPVCor(goodCells, currentPFCellArray, concurrentCellArray, "euclidianDistance");
+            cosineSim = getPVCor(goodCells, currentPFCellArray, concurrentCellArray, "cosineSim");
+            
+            % We can add those to our struct
+            
+            allLaps = [allLaps; struct("lap", {lap}, "pvCorrelation", {pvCorrelation}, "euclidianDistance", {euclidianDistance}, "cosineSim", {cosineSim})];
+            
         end
-
-        % We get the cell array with PF for each neuron
-
-        currentPFCellArray = currentPFData{lap}.raw;
-        concurrentCellArray = concurrentPFData{lap}.raw;
         
-        % We find all the cells that are good place cells in one of the
-        % tracks
-        
-        goodCells = concurrentPFData{lap}.good_cells;
-        
-        % We get the correlation vector for the current lap
-        pvCorrelationVector = getPVCor(goodCells, currentPFCellArray, concurrentCellArray, mode);
-        % And add its mean to the corr_vector
-        corr_vector(lap) = mean(pvCorrelationVector);
-
-        
+        % Now we can save everything in our meta-struct
+    
+        population_vector_laps = [population_vector_laps ; struct("animal", {animalOI}, "condition", {conditionOI}, "day", {dayOI}, ...
+                       "track", {track}, "allLaps", {allLaps}, "finalPlaceField", {finalPlaceField})];
+    
     end
-
 end
 
-
-
-%% Function to compute the population vector correlation
-
-function [pvCorrelationVector] = getPVCor(goodCells, currentPFCellArray, concurrentCellArray, mode)
-
-    % Subsample the cell arrays based on the good cells
-    currentPFCellArray = currentPFCellArray(goodCells);
-    concurrentCellArray = concurrentCellArray(goodCells);
-    
-    % Depending on MODE, we define a concurrentVector
-    if mode == "LAST"
-        concurrentVector = concurrentCellArray(end).raw;
-    elseif mode == "FIRST"
-        concurrentVector = concurrentCellArray(1).raw;
-    elseif mode == "ALL"
-        concurrentVector = concurrentCellArray.raw;
-    end
-    
-    % If mode == "NEXT", we use another way
-
-    if mode ~= "NEXT" % For all modes except NEXT
-
-        % Compute the correlation for each pair of cells
-        correlationCoefficients = cellfun(@(x) corrcoef(x, concurrentVector), currentPFCellArray, 'UniformOutput', false);
-
-    else % For NEXT - return correlation vector length is -1 (last half-lap excluded)
-
-        % We offset concurrentCellArray by 1 - 2 -> End
-        concurrentCellArray(1) = [];
-        % We offset currentPFCellArray by 1 - 1 -> End - 1
-        currentPFCellArray(end) = [];
-
-        correlationCoefficients = cellfun(@(x, y) corrcoef(x, y), currentPFCellArray, concurrentCellArray, 'UniformOutput', false);
-
-    end
-
-    % Extract the correlation coefficients and returns it
-    pvCorrelationVector = cellfun(@(x) x(2), correlationCoefficients);
-
-
-end
+% save(PATH.SCRIPT + "\..\Data\extracted_activity_mat_lap.mat", "activity_mat_laps");
