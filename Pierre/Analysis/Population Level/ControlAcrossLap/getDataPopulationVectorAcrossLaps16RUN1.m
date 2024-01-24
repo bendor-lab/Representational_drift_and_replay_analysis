1633f5ed1f75474f63382564494b31c542ca59e8
Pierre Var <78429558+tarsky5@users.noreply.github.com>
2024-01-24 10:03:10 +0000

    corected the PV correlation measure

---
+++ Pierre/Analysis/Population Level/ControlAcrossLap/getDataPopulationVectorAcrossLaps16RUN1.m
@@ -0,0 +1,98 @@
+% This script generates a file containing : 
+% - Animal ID
+% - Condition
+% - Day
+% - Lap
+% - PV correlation with lap 16 - RUN1
+% - Euclidian distance with lap 16 - RUN1
+% - cosine similarity with lap 16 - RUN1
+
+% This file is only treating Track 1 / 3 data
+% Currently in progress
+
+clear
+
+% PATH things
+PATH.SCRIPT = fileparts(mfilename('fullpath'));
+cd(PATH.SCRIPT)
+
+sessions = data_folders_excl; % Use the function to get all the file paths
+
+population_vector_laps = struct("animal", {}, "condition", {}, "day", {}, ...
+                       "track", {}, "allLaps", {}, "testPlaceField", {});
+                   
+allLaps = struct("lap", {}, "pvCorrelation", {}, "euclidianDistance", {}, "cosineSim", {});
+                              
+% We iterate through files
+for cfile = sessions
+    disp(cfile);
+    file = cfile{1};
+    
+    [animalOI, conditionOI, dayOI] = parseNameFile(file); % We get the informations about the current data
+
+    %% Variable loading
+    % We load the place fields computed per lap for each animal - lap_place_fields
+    load(file + "\extracted_lap_place_fields");
+    % And a general PF data to check if good cell on the whole track
+    load(file + "\extracted_place_fields");
+    load(file + "\extracted_laps"); % Import table lap_times
+    load(file + "\extracted_clusters"); % Import table clusters
+    load(file + "\extracted_waveforms"); % Import allclusters_waveform
+    load(file + "\extracted_position"); % Import position
+    
+    %% We get the template place field : L16 RUN1
+    
+    testPlaceField = lap_place_fields(1).Complete_Lap{end};
+    
+    % For population vector analysis, we only use the good place cells of
+    % the FPF - cells that will become good cells
+    
+    goodCells = testPlaceField.good_cells;
+       
+    % We iterate over tracks 1 and 3
+    for track = 1:2:3
+        
+        % We find the number of laps
+        nbLaps = min([lap_times(track).number_completeLaps, length(lap_place_fields(track).Complete_Lap)]);
+        listLaps = 1:nbLaps;
+        
+        % We create the struct to store the data per lap
+        
+        allLaps = struct("lap", {}, "pvCorrelation", {}, "euclidianDistance", {}, "cosineSim", {});       
+        
+        % We iterate through laps
+        for lap = listLaps
+            
+            % We get the relevant data regarding place fields
+            goodPFData = lap_place_fields(track).Complete_Lap{lap};
+            
+            currentPFCellArray = goodPFData.raw;
+            concurrentCellArray = testPlaceField.raw;
+            
+            % rare case : if not the same resolution, for now we ignore
+            if(length(currentPFCellArray{1}) ~= length(concurrentCellArray{1}))
+                continue;
+            end
+            
+            % We find all the goodCells of the FPF that are in 
+            
+            % We compute the PV correlation
+            pvCorrelation = getPVCor(goodCells, currentPFCellArray, concurrentCellArray, "pvCorrelation");
+            euclidianDistance = getPVCor(goodCells, currentPFCellArray, concurrentCellArray, "euclidianDistance");
+            cosineSim = getPVCor(goodCells, currentPFCellArray, concurrentCellArray, "cosineSim");
+            
+            % We can add those to our struct
+            
+            allLaps = [allLaps; struct("lap", {lap}, "pvCorrelation", {pvCorrelation}, "euclidianDistance", {euclidianDistance}, "cosineSim", {cosineSim})];
+            
+        end
+        
+        % Now we can save everything in our meta-struct
+    
+        population_vector_laps = [population_vector_laps ; struct("animal", {animalOI}, "condition", {conditionOI}, "day", {dayOI}, ...
+                       "track", {track}, "allLaps", {allLaps}, "testPlaceField", {testPlaceField})];
+    
+    end
+end
+
+save(PATH.SCRIPT + "\..\..\..\Data\population_vector_lapsLap16RUN1.mat", "population_vector_laps");
--- Pierre/Utilitary/Population_level/getPVCor.m
+++ Pierre/Utilitary/Population_level/getPVCor.m
@@ -7,18 +7,30 @@ function [result] = getPVCor(goodCells, currentPFCellArray, concurrentCellArray,
     currentPFCellArray = currentPFCellArray(goodCells);
     concurrentCellArray = concurrentCellArray(goodCells);
     
+    % We reverse the data XBIN -> cells for population vector
+    
+    currentPFXBins = repelem({0}, length(currentPFCellArray{1}));
+    concurrentPFYBins = repelem({0}, length(currentPFCellArray{1}));
+    
+    for xbin = 1:length(currentPFCellArray{1})
+        allCurrentCellAct = cellfun(@(x) x(xbin), currentPFCellArray);
+        allConcurrentCellAct = cellfun(@(x) x(xbin), concurrentCellArray);
+        
+        currentPFXBins(xbin) = {allCurrentCellAct};
+        concurrentPFYBins(xbin) = {allConcurrentCellAct};
+    end
+    
     if mode == "pvCorrelation"
-        correlationCoefficients = cellfun(@(x, y) corrcoef(x, y), currentPFCellArray, concurrentCellArray, 'UniformOutput', false);
-        % Extract the correlation coefficients and returns it
-        result = mean(cellfun(@(x) x(2), correlationCoefficients), "omitnan");
+        corVector = cellfun(@(x, y) corrcoef(x, y), currentPFXBins, concurrentPFYBins, 'UniformOutput', false);
+        corVector = cellfun(@(x) x(2, 1), corVector);
+        result = mean(corVector, "omitnan");
     elseif mode == "euclidianDistance"
-        result = cellfun(@(x, y) norm(x - y), currentPFCellArray, concurrentCellArray, 'UniformOutput', false);
+        result = cellfun(@(x, y) norm(x - y), currentPFXBins, concurrentPFYBins, 'UniformOutput', false);
         result = mean(cell2mat(result), "omitnan");
     else
         % We compute the cosine similarity
-        cosine_similarity = cellfun(@(x, y) dot(x, y)/(norm(x)*norm(y)), currentPFCellArray, concurrentCellArray, 'UniformOutput', false);
+        cosine_similarity = cellfun(@(x, y) dot(x, y)/(norm(x)*norm(y)), currentPFXBins, concurrentPFYBins, 'UniformOutput', false);
         result = mean(cell2mat(cosine_similarity), "omitnan");
     end
-
 
 end
