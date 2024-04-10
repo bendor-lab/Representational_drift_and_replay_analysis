4b160d817ac271562fd1e788b8c7a20fa256de5e
Pierre Var <78429558+tarsky5@users.noreply.github.com>
2024-04-10 15:11:32 +0100

    rerun_file_phase

--- Replay analysis/ExtractAllT2vsT4.m
+++ Replay analysis/ExtractAllT2vsT4.m
@@ -1,7 +1,8 @@
 % Long analysis : extraction of all the replay events for T2 vs. T4
+clear
 
 folders = data_folders_excl;
-tracks_compared = [2, 4];
+tracks_compared = [1, 3];
 
 folders = folders(1:end);
 
