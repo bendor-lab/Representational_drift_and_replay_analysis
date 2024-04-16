e1bcffc8655e373afe73f0bcb28bbf4c5c8eca83
Pierre Var <78429558+tarsky5@users.noreply.github.com>
2024-04-16 16:30:56 +0100

    rerun split analysis tuning

--- Pierre/Analysis/Cell Level/Phase_SWR/generateDataPhaseSavingsNSIG.m
+++ Pierre/Analysis/Cell Level/Phase_SWR/generateDataPhaseSavingsNSIG.m
@@ -29,7 +29,7 @@ allTuningMat.tuningMat = {};
 
 %% Extraction & computation
 
-for fileID = 1:length(sessions)
+parfor fileID = 1:length(sessions)
 
     disp(fileID);
     file = sessions{fileID}; % We get the current session
@@ -167,5 +167,5 @@ condition = str2double(condition);
 
 phase_data = table(sessionID, animal, condition, track, cell, refinCM, refinFR, refinPeak, meanPhase, phaseLocking, significance, label);
 
-save("phase_data_NSIG.mat", "phase_data");
+save("phase_data_NSIG_split.mat", "phase_data");
 % save("tuning_curves_sig", "allTuningMat");
