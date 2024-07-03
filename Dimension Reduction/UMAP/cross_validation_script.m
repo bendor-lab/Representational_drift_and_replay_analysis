fb31a29fedc2594b1231720e7f6f18619f27cc7b
Pierre Var <78429558+tarsky5@users.noreply.github.com>
2024-07-03 13:18:59 +0100

    new files analysis dim

--- Dimension Reduction/UMAP/cross_validation_script.m
+++ Dimension Reduction/UMAP/cross_validation_script.m
@@ -2,116 +2,121 @@
 
 clear
 
-currentPath = data_folders_excl_legacy;
-currentPath = currentPath{4};
+for session_n = 1:numel(data_folders_excl_legacy)
 
-% Load the data
+    disp(session_n);
 
-load(currentPath + "/extracted_laps");
-load(currentPath + "/extracted_clusters");
-load(currentPath + "/extracted_place_fields_BAYESIAN");
-load(currentPath + "/extracted_position");
+    currentPath = data_folders_excl_legacy;
+    currentPath = currentPath{session_n};
 
-trackOI = 1;
+    % Load the data
 
-positionArray = position.linear(trackOI).linear;
+    load(currentPath + "/extracted_laps");
+    load(currentPath + "/extracted_clusters");
+    load(currentPath + "/extracted_place_fields_BAYESIAN");
+    load(currentPath + "/extracted_position");
 
-% Start and end times of the whole session
+    trackOI = 1;
+    positionArray = position.linear(trackOI).linear;
 
-startTime = lap_times(trackOI).completeLaps_start(1);
-endTime = lap_times(trackOI).completeLaps_stop(end);
+    % Start and end times of the whole session
 
-timePositionBool = position.t <= endTime & position.t >= startTime;
-positionArray = positionArray(timePositionBool);
+    startTime = lap_times(trackOI).completeLaps_start(1);
+    endTime = lap_times(trackOI).completeLaps_stop(end);
 
-binEdges = startTime:0.100:endTime + 0.100;  % Create bins of timeBin intervals
+    timePositionBool = position.t <= endTime & position.t >= startTime;
+    positionArray = positionArray(timePositionBool);
 
-x = interp1(position.t(timePositionBool), positionArray, binEdges(1:end-1), 'nearest');
+    binEdges = startTime:0.100:endTime + 0.100;  % Create bins of timeBin intervals
 
-% We create a big histogram of each spike of each cell
+    x = interp1(position.t(timePositionBool), positionArray, binEdges(1:end-1), 'nearest');
 
-allCells = unique(clusters.spike_id);
-histBinned = zeros(length(allCells), length(binEdges) - 1);
+    % We create a big histogram of each spike of each cell
 
-for cID = 1:length(allCells)
-    currentCell = allCells(cID);
-    allSpikeTimes = clusters.spike_times(clusters.spike_id == currentCell);
+    allCells = unique(clusters.spike_id);
+    histBinned = zeros(length(allCells), length(binEdges) - 1);
 
-    % Get the count
-    spikeCounts = histcounts(allSpikeTimes, binEdges);
+    for cID = 1:length(allCells)
+        currentCell = allCells(cID);
+        allSpikeTimes = clusters.spike_times(clusters.spike_id == currentCell);
 
-    % add to the histBinned
-    histBinned(cID, :) = spikeCounts;
+        % Get the count
+        spikeCounts = histcounts(allSpikeTimes, binEdges);
+
+        % add to the histBinned
+        histBinned(cID, :) = spikeCounts;
+    end
+
+    % We filter the hist with only good place cells
+    % histBinned = histBinned(place_fields_BAYESIAN.good_place_cells, :);
+
+    % We smooth using a 500 ms gaussian kernel
+    histBinned = smoothdata(histBinned, 2, "gaussian", 5);
+
+    % We speed filter
+
+    % Get the speed of the animal at each time
+    position_speed = abs(position.v_cm);
+    % Get the speed of the animal at every timebin
+    interp_speed = interp1(position.t(timePositionBool), position_speed(timePositionBool), ...
+        binEdges, 'nearest');
+    % NaN the rows of the hist where the speed is less than 5 cm / s
+    histBinned(:, interp_speed < 5) = NaN;
+
+    % We remove all the times in the hist that are not running
+    % RunningTimes contains only time values inside laps
+    % Note : Speed Filter does the job usually
+
+    runningTimes = zeros(1, length(binEdges) - 1);
+    lapVector = zeros(1, length(binEdges) - 1);
+
+    direction = 1;
+    halfLaps = direction:2:length(lap_times(trackOI).halfLaps_start);
+    nbLaps = numel(halfLaps);
+
+    nb_clusters = nbLaps;
+    labels = floor(halfLaps * (nb_clusters/2) / nbLaps) + 1;
+
+    for lid = 1:nbLaps
+
+        half_lap = halfLaps(lid);
+        label = labels(lid);
+
+        runningTimes(binEdges <= lap_times(trackOI).halfLaps_stop(half_lap) & ...
+            binEdges >= lap_times(trackOI).halfLaps_start(half_lap)) = 1;
+
+        lapVector(binEdges <= lap_times(trackOI).halfLaps_stop(half_lap) & ...
+            binEdges >= lap_times(trackOI).halfLaps_start(half_lap)) = label;
+
+    end
+
+    idleTimes = not(runningTimes);
+
+    %  We NaN those times in the hist
+    histBinned(:, idleTimes) = NaN;
+    lap = lapVector';
+
+    x = x';
+
+    % Now we can pivot the hist, add info and export to csv
+
+    histBinned = histBinned';
+    % cellLabel = place_fields_BAYESIAN.good_place_cells;
+    cellLabel = allCells;
+
+    t = table(x, lap);
+
+    for c = 1:numel(cellLabel)
+        cell = cellLabel(c);
+        lab = "c" + cell;
+        values = histBinned(:, c);
+
+        t.(lab) = values;
+    end
+
+    t = rmmissing(t);
+
+    writetable(t, "data_cross_validation/data_" + ...
+        num2str(session_n) + "_T" + num2str(trackOI)+ ".csv")
+
 end
-
-% We filter the hist with only good place cells
-% histBinned = histBinned(place_fields_BAYESIAN.good_place_cells, :);
-
-% We smooth using a 500 ms gaussian kernel
-histBinned = smoothdata(histBinned, 2, "gaussian", 5);
-
-% We speed filter
-
-% Get the speed of the animal at each time
-position_speed = abs(position.v_cm);
-% Get the speed of the animal at every timebin
-interp_speed = interp1(position.t(timePositionBool), position_speed(timePositionBool), ...
-                       binEdges, 'nearest');
-% NaN the rows of the hist where the speed is less than 5 cm / s
-histBinned(:, interp_speed < 5) = NaN;
-
-% We remove all the times in the hist that are not running
-% RunningTimes contains only time values inside laps
-% Note : Speed Filter does the job usually
-
-runningTimes = zeros(1, length(binEdges) - 1);
-lapVector = zeros(1, length(binEdges) - 1);
-
-direction = 1;
-halfLaps = direction:2:length(lap_times(trackOI).halfLaps_start);
-nbLaps = numel(halfLaps);
-
-nb_clusters = nbLaps;
-labels = floor(halfLaps * (nb_clusters/2) / nbLaps) + 1;
-
-for lid = 1:nbLaps
-
-    half_lap = halfLaps(lid);
-    label = labels(lid);
-
-    runningTimes(binEdges <= lap_times(trackOI).halfLaps_stop(half_lap) & ...
-        binEdges >= lap_times(trackOI).halfLaps_start(half_lap)) = 1;
-
-    lapVector(binEdges <= lap_times(trackOI).halfLaps_stop(half_lap) & ...
-        binEdges >= lap_times(trackOI).halfLaps_start(half_lap)) = label;
-
-end
-
-idleTimes = not(runningTimes);
-
-%  We NaN those times in the hist
-histBinned(:, idleTimes) = NaN;
-lap = lapVector';
-
-x = x';
-
-% Now we can pivot the hist, add info and export to csv
-
-histBinned = histBinned';
-% cellLabel = place_fields_BAYESIAN.good_place_cells;
-cellLabel = allCells;
-
-t = table(x, lap);
-
-for c = 1:numel(cellLabel)
-    cell = cellLabel(c);
-    lab = "c" + cell;
-    values = histBinned(:, c);
-
-    t.(lab) = values;
-end
-
-t = rmmissing(t);
-
-writetable(t, "neuralData_cross_validation.csv")
-
