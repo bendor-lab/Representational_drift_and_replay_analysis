222ae42b58b401271a4f6509251f3038f38dfe64
Pierre Var <78429558+tarsky5@users.noreply.github.com>
2024-07-27 17:15:35 +0200

    foo

--- Pierre/Analysis/Population Level/Awake_Improvements/improvements_lookup.m
+++ Pierre/Analysis/Population Level/Awake_Improvements/improvements_lookup.m
@@ -48,6 +48,8 @@ for cID = 1:19
     end
 end
 
+mean_data.pvCorrExp = 2.^mean_data.pvCorr;
+
 time_ser = mean_data([], :);
 
 % Calculate the derivative of the pv-correlation across laps
@@ -67,10 +69,14 @@ for cID = 1:19
         end
 
         pv_cor = current_data.pvCorr;
+        pv_cor_exp = current_data.pvCorrExp;
+
         pv_evolution = diff(pv_cor);
+        pv_exp_evolution = diff(pv_cor_exp);
 
         snippet = current_data(1:end-1, :);
         snippet.pvCorr = pv_evolution; %pvCorr now refer to delta pv
+        snippet.pvCorrExp = pv_exp_evolution;
 
         time_ser = [time_ser; snippet];
     
@@ -80,10 +86,9 @@ end
 
 %%
 
-isFirstLap = (time_ser.lap == 5 | time_ser.lap == 6) & time_ser.exposure == 1 ...
-              & time_ser.condition == 16;
+isFirstLap = time_ser.lap == 1 & time_ser.exposure == 1;
 
-all_pv = time_ser.pvCorr(isFirstLap);
+all_pv = time_ser.pvCorrExp(isFirstLap);
 all_SWR = time_ser.idleSWR(isFirstLap);
 all_Idle = time_ser.idlePeriod(isFirstLap);
 all_rep = time_ser.idleReplay(isFirstLap);
@@ -97,12 +102,11 @@ corrplot(foo)
 
 all_cor = [];
 
-for lap = 2:16
-    current_sub = (time_ser.lap == lap*2-1 | time_ser.lap == lap*2) & time_ser.exposure == 1 ...
-                   & time_ser.condition == 16;
+for lap = 1:15
+    current_sub = time_ser.lap == lap & time_ser.exposure == 1;
 
-    all_pv = time_ser.pvCorr(current_sub);
-    all_theta = time_ser.thetaCycles(current_sub);
+    all_pv = time_ser.pvCorrExp(current_sub);
+    all_theta = time_ser.idleSWR(current_sub);
 
     corr_coef = corrcoef(all_pv, all_theta);
     all_cor(end + 1) = corr_coef(2, 1);
@@ -111,3 +115,63 @@ end
 
 plot(all_cor)
 grid on;
+fitlme(time_ser(time_ser.exposure == 1, :), 'pvCorrExp ~ idleSWR + idlePeriod + idleReplay + thetaCycles')
+
+%%
+
+sum_data = groupsummary(mean_data, ["lap", "exposure"], "median", ["pvCorr", "idlePeriod", "idleReplay", "idleSWR", "thetaCycles"]);
+
+cExp = 2;
+
+subplot(2, 2, 1)
+boxchart(mean_data.lap(mean_data.exposure == cExp), ...
+         mean_data.idlePeriod(mean_data.exposure == cExp))
+grid on;
+title("Idle period after lap - RUN2")
+ylabel("Seconds")
+xlabel("Lap")
+
+hold on;
+plot(sum_data.lap(sum_data.exposure == cExp), sum_data.median_idlePeriod(sum_data.exposure == cExp), 'r')
+
+subplot(2, 2, 2)
+boxchart(mean_data.lap(mean_data.exposure == cExp), ...
+         mean_data.idleSWR(mean_data.exposure == cExp))
+grid on;
+
+title("Number of SWR - RUN2")
+ylabel("n° of SWR")
+xlabel("Lap")
+
+hold on;
+plot(sum_data.lap(sum_data.exposure == cExp), sum_data.median_idleSWR(sum_data.exposure == cExp), 'r')
+
+
+subplot(2, 2, 3)
+boxchart(mean_data.lap(mean_data.exposure == cExp), ...
+         mean_data.idleReplay(mean_data.exposure == cExp))
+grid on;
+
+title("Number of awake replay - RUN2")
+ylabel("n° of replay")
+xlabel("Lap")
+
+hold on;
+plot(sum_data.lap(sum_data.exposure == cExp), sum_data.median_idleReplay(sum_data.exposure == cExp), 'r')
+
+
+subplot(2, 2, 4)
+boxchart(mean_data.lap(mean_data.exposure == cExp), ...
+         mean_data.thetaCycles(mean_data.exposure == cExp))
+grid on;
+
+title("Number of theta cycles - RUN2")
+ylabel("n° of theta cycles")
+xlabel("Lap")
+
+hold on;
+plot(sum_data.lap(sum_data.exposure == cExp), sum_data.median_thetaCycles(sum_data.exposure == cExp), 'r')
+
+%%
+
+scatter(mean_data.lap(mean_data.exposure == 1), mean_data.pvCorrExp(mean_data.exposure == 1), 'r')
