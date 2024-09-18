90fc6eef8bc0b17ff659db1f03325b0127e3a0e2
Pierre Var <78429558+tarsky5@users.noreply.github.com>
2024-09-18 14:36:34 +0100

    control 2

--- Pierre/Analysis/Population Level/Statistics/generateTimeSeriesOLap.m
+++ Pierre/Analysis/Population Level/Statistics/generateTimeSeriesOLap.m
@@ -4,8 +4,8 @@ clear
 PATH.SCRIPT = fileparts(mfilename('fullpath'));
 cd(PATH.SCRIPT)
 
-% sessions = data_folders_excl; % We fetch all the sessions folders paths
-sessions = data_folders_deprivation;
+sessions = data_folders_excl; % We fetch all the sessions folders paths
+% sessions = data_folders_deprivation;
 
 % We create identifiers for cells from each session.
 % Format is : IDENT-00-XXX
@@ -35,7 +35,7 @@ diffSum = @(x1, x2) abs(x1 - x2)/(x1 + x2);
 
 %% Extraction & computation
 
-for fileID = 2:length(sessions)
+parfor fileID = 1:length(sessions)
 
     disp(fileID);
     file = sessions{fileID}; % We get the current session
